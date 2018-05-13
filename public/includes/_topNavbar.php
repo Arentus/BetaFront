2a023afe27216cc19bc173430e2603b00c1fa019
Robwert Mota <robwert7991@gmail.com>
2018-05-13 00:50:38 -0400

    [SASS][PHP] added search bar and navbar dropdown

---
+++ public/includes/_topNavbar.php
@@ -0,0 +1,120 @@
+ <div class="collaxbar">
+        <div class="wrappersearch" id="divs">
+            <div class="searchdropdown">
+                <form>
+                  <input class="searchbar" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-lg btn-outline-defaultr" type="submit">Search</button>
+                </form>
+            </div>  
+        </div>
+    </div>
+    
+
+    <div class="container-fluid"> 
+        <!--search div-->
+        
+        <!---->
+        <nav class="navbar navbar-expand navbar-light p-0 d-flex">
+
+        <div class="btn btn-sm searchbtn navbar-nav">   
+            <i class="fas fa-search hoverable"></i>    
+        </div>
+
+            <ul class="navbar-nav">
+                <li class="nav-item">         
+                    <div class="dropdown">   
+                       
+                        <button class="nav-item btnlang rounded" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">English</button>
+                      
+                      <div class="dropdown-menu langdrop" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Spanish</a>
+                        <a class="dropdown-item" href="#">French</a>
+                        <a class="dropdown-item" href="#">Germany</a>
+                      </div>
+                    </div>
+                </li>
+            </ul>
+
+            <div class="" id="navbarTogglerDemo01">
+                <ul class="navbar-nav">
+                    <li class="nav item">
+                          <a href="#" class="link">Contact</a>
+                    </li>
+               </ul>
+          </div>
+        </nav>
+        <!--end topNavbar-->                
+        <!--Navbar Main-->
+        
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+         <a class="navbar-brand" href="#"><img src="assets/img/cropped-ds-ico.png" width="30" height="30" alt="">
+            BetaFront</a>
+      </li>
+
+
+
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          About
+        </a>
+        <div class="dropdown-dropped" aria-labelledby="navbarDropdownMenuLink">
+          
+          <div class="wrapperflex">
+
+            <div class="d-item pic-side flex-fill p-1 ">
+              <img class="img-nav" src="../assets/img/workplace.jpg">
+            </div>
+            
+            <div class="dropdown-links flex-fill p-2">
+              <h5>About Us</h5>
+              <a class="d-item" href="#">Action</a>
+              <a class="d-item" href="#">Another action</a>
+              <a class="d-item" href="#">Something else here</a>
+            </div>
+          </div>
+        </div>
+      </li>
+      
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Plans
+        </a>
+        <div class="dropdown-dropped" aria-labelledby="navbarDropdownMenuLink">
+          
+          <div class="wrapperflex">
+            
+            <div class="dropdown-links flex-fill p-2">
+              <h5>Plans</h5>
+              <a class="d-item" href="#">Action</a>
+              <a class="d-item" href="#">Another action</a>
+              <a class="d-item" href="#">Something else here</a>
+            </div>
+          </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+          Dropdown link
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
--- public/index.php
+++ public/index.php
@@ -1,66 +1,55 @@
 <!doctype html>
 <html lang="es">
-
-	<!--
-	|
-	|	@author Robwert Mota
-	|	@date 10 de mayo de 2018
-	|	@name BetaFront
-	|	@description Sitio web simple de ejemplo para mi portafolio con Bootstrap 4.1
-	|
-	|
-	-->
-
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 
-    <title>Venmed</title>
+    <link rel="stylesheet" type="text/css" href="css/vendor/bootstrap.css">  
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+    <title>BetaFront</title>
   </head>
   <body>
+  
     <header>
-    	<!--topNavbar-->
-    	<nav>		
-		  	<div class="container-fluid">
-		  		<small>Topnavbar</small>
-
-		  		<select>
-		  			<option>English</option>
-		  			<option>Spanish</option>
-		  			<a href="#">Contact</a>
-		  			<form action="#" method="post">
-		  				<input type="search" name="search" placeholder="search bar">
-		  				<input type="submit" name="submit" value="Search">
-		  			</form>
-		  			<a href="#">Sign Up</a>
-		  		</select>
-		  	</div>	
-    	</nav>
-    	<!--end topNavbar-->
-
-    	<!--Navbar Main-->
-    	<nav>
-    		<small>Navbar</small>
-
-    	</nav>
-    </header>
-    	<!--end Navbar-->
+    <!--topNavbar-->   
+    <?php include_once('includes/_topNavbar.php'); ?>
+    <!--end Navbar-->
+        
+    </header>  
+       
     <!--Lading Page-->
-    <section>
-    	<div class="container-fluid">
-    		 <h1>Landing PAGE</h1>
-    	</div>
-    </section>
-	
+
+        <section>
+        	<div class="container-fluid">
+        		 <h1>Landing PAGE</h1>
+        	</div>
+        </section>
+    	
+    </div>
+
+
+       
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/vendor/jquery-3.3.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+        
+        $(document).ready(function(){
+            
+             $( ".searchbtn" ).click(function() {
+              $( ".collaxbar" ).slideToggle("slow");
+            });
+        });
+    </script>
   </body>
 </html>
